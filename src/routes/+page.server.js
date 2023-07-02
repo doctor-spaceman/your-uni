a30e9fd56a6cd73b12ad19cb0e4275c58c53ed62
Matt McLean <9052437+doctor-spaceman@users.noreply.github.com>
2023-07-01 19:50:02 -0700

    Adjust imports to satisfy build

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,4 @@
-import { parse } from 'papaparse'
+import Papa from 'papaparse'
 
 export async function load({ fetch }) {
   const response = await fetch('https://gist.githubusercontent.com/simonlast/d5a86ba0c82e1b0d9f6e3d2581b95755/raw/f608b9b896dd3339df13dae317998d5f24c00a50/edu-scorecard.csv')
@@ -8,7 +8,7 @@ export async function load({ fetch }) {
     const csvOptions = {
       header: true
     }
-    const parsedData = parse(data, csvOptions)
+    const parsedData = Papa.parse(data, csvOptions)
 
     return {
       units: parsedData.data.map((entry) => ({
